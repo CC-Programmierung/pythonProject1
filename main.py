e812bd7f50e13555e8dbfec820b0499fd9bdc036
DonCerebro1 <CMollerfriedrich@gmail.com>
2022-05-03 12:31:48 +0200

    init

---
+++ main.py
@@ -0,0 +1,41 @@
+# Dies ist ein Zeilenkommentarr - Dieserkann einzeln in einer Zeile stehen
+
+"""
+Dies ist ein Blockkommentar
+Er erstrekt sich über mehrere Zeilen. Alles zwischen 3 Anführungzeichen wird als Kommentar interpretiert
+"""
+
+#print("Hello World")
+
+"""
+Bei diesem Program wird nun zunächst der String Wert "Hello World" in einer Variable mit dem Namen "hw" geschrieben
+"""
+#hw = "Hallo Welt"
+#print(hw)
+
+"""
+Benutzereingabe
+"""
+#variable = input('Bitte geben Sie etwas ein und bestätigen mit der "Enter-Taste"')
+#print("Folgendes haben Sie eingeben:")
+#print(variable)
+
+"""
+Berechnungen erkennt er als String und nicht als Zahl dafür fehlt das casten zu integer
+"""
+#summe = 5 + 4
+#print(5+3)
+#print(summe)
+
+#number1 = input("1:")
+#number2 = input("2:")
+#summe = number1 + number2
+#print(summe)
+"""
+mit dem Cast zu int erkennt der compiler das es sich um eine zahl handelt. Sonst erkennt er die Zahl
+wie oben als Buchstaben
+"""
+#number1 = int (input("1:"))
+#number2 = int (input("2:"))
+#summe = number1 + number2
+#print(summe)
